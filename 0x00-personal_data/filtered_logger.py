9cd39dc1538e0c4217a6aca463a40fa9e47f45dd
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-31 06:21:30 +0000

    adding type annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,19 +6,9 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
-    """
-    Obfuscates specified fields in a log message.
-
-    Args:
-        fields (list of str): List of field names to be obfuscated.
-        redaction (str): String to replace the field values with.
-        message (str): The original log message containing the data.
-        separator (str): Character used to separate fields in the message.
-
-    Returns:
-        str: The log message with specified fields obfuscated.
-    """
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscates specified fields in a log message."""
     for field in fields:
         message = re.sub(f"{field}=[^{separator}]*", f"{field}={redaction}",
                          message)

aea97cb7db735f790f832fef6b430b7fee947b60
Gbeminiyi Samagbeyi <111027908+Gbeminiyi-S@users.noreply.github.com>
2023-08-31 08:44:25 +0100

    Documenting __init__

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -25,6 +25,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: list):
+        """initializing variables"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 

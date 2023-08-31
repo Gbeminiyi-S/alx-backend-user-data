09fb9f566c58d9b451dedae68cfa755c475e7cdb
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-31 07:10:58 +0000

    Task 1 - solution

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
     in log messages.
 """
 
+import logging
 import re
 from typing import List
 
@@ -14,3 +15,20 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f"{field}=[^{separator}]*", f"{field}={redaction}",
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records using filter_datum """
+        msg = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)

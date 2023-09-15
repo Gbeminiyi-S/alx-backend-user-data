e44ce1230484747c3168e72ea25667c6b02e22ee
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2023-09-15 21:52:59 +0100

    Task 4 - solution

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,9 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if authentication is required for a given path."""
-        return False
+        if path not in excluded_paths or path is None or
+        excluded_paths is None or excluded_paths == []:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """Extracts the authorization header from the request."""

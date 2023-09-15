a2cfb0ea758a9f4dde2a2165cb57b2923f112573
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2023-09-15 21:40:57 +0100

    adding comment to class Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,8 @@ from flask import request
 from typing import List, TypeVar
 
 class Auth:
+    """Provides methods for managing API authentication."""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if authentication is required for a given path."""
         return False
